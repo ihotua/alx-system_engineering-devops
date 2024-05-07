07a2f7f0e1ae827d0e1ccf693ebb6e39258f567e
root <ihotuagbo@gmail.com>
2024-05-07 15:23:08 -0700

    Processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,34 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Creates 5 zombie processes
+ * infinite_while - prevents process from ending to make it a zombie
  *
- * Return: 0
+ * Return: Always 0
  */
+
+int infinite_while(void)
+{
+	while (1)
+		sleep(1);
+	return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ *
+ * Return: 0 on success
+ */
+
 int main(void)
 {
- pid_t pid;
- int i;
+	pid_t pikin;
+	int i;
 
- for (i = 0; i < 5; i++)
- {
-  pid = fork();
-  if (pid < 0)
-  {
-   perror("Error:");
-   exit(1);
-  }
-  else if (pid == 0)
-  {
-   printf("Zombie process created, PID: %d\n", getpid());
-   exit(0);
-  }
-  else
-  {
-   sleep(1);
-  }
- }
- return (0);
+	for (i = 0; i < 5; i++)
+	{
+		pikin = fork();
+		if (pikin < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pikin == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n",
+					pikin);
+		}
+	}
+	infinite_while();
+	return (0);
 }
