d9be152d9f0c662779dcdd226162c7622ea0fefa
root <ihotuagbo@gmail.com>
2024-05-07 15:07:55 -0700

    Processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -7,29 +7,28 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
-    pid_t pid;
-    int i;
+ pid_t pid;
+ int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("Error:");
-            exit(1);
-        }
-        else if (pid == 0)
-        {
-            printf("Zombie process created, PID: %d\n", getpid());
-            exit(0);
-        }
-        else
-        {
-            sleep(1);
-        }
-    }
-    return (0);
+ for (i = 0; i < 5; i++)
+ {
+  pid = fork();
+  if (pid < 0)
+  {
+   perror("Error:");
+   exit(1);
+  }
+  else if (pid == 0)
+  {
+   printf("Zombie process created, PID: %d\n", getpid());
+   exit(0);
+  }
+  else
+  {
+   sleep(1);
+  }
+ }
+ return (0);
 }
