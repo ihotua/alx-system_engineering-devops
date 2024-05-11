c7bcfc383da681485e87cebb10cc21f739fc8229
root <ihotuagbo@gmail.com>
2024-05-11 14:00:31 -0700

    Regular Xpressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
-from = ARGV[0].scan(/from:(.*?)\]/)
-to = ARGV[0].scan(/to:(.*?)\]/)
-flags = ARGV[0].scan(/flags:(.*?)\]/)
-msg = ARGV[0].scan(/msg:(.*?)\]/)
-udh = ARGV[0].scan(/udh:(.*?)\]/)
-puts [from, to, flags, msg, udh].join(',')
+
+from = ARGV[0].scan(/from:(.*?)\]/).join(',')
+to = ARGV[0].scan(/to:(.*?)\]/).join(',')
+flags = ARGV[0].scan(/flags:(.*?)\]/).join(',')
+puts "#{from},#{to},#{flags}"
