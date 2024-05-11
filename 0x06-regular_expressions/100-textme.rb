d315a00a248975344061b8c5989c94cef906f045
root <ihotuagbo@gmail.com>
2024-05-11 12:57:28 -0700

    Regular Xpressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(\d+)\]|\bto:(\d+)\]|\bflags:([^\]]+)/).flatten.compact.join(',')
+puts ARGV[0].scan(/from:(?:\+)?(\d+)\]|\bto:(?:\+)?(\d+)\]|\bflags:([^\]]+)/).flatten.compact.join(',')
