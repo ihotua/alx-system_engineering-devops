135e194fb19839772fda8b117c7b3220c0f1b37c
root <ihotuagbo@gmail.com>
2024-07-18 11:25:53 -0700

    Web Server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,15 +7,15 @@ package { 'nginx':
 file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server
+  after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/@sabigurl permanent;',
 }
 
 file { '/var/www/html/index.html':
-  content => "Hello World',
+  content => "Hello World!',
 }
 
 service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
+  require => Package['nginx'],
 }
